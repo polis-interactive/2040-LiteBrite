01cb1984027e87fdb1192bc46457199e22df52d1
broose-goose <bruce@polis.tv>
2023-07-12 00:26:34 -0500

    Okay, moving to static frontend hosted on ngnix with fully decoupled backend. Api routes will be just that; api routes

--- litebrite-backend/cmake/crow.cmake
+++ litebrite-backend/cmake/crow.cmake
@@ -1,4 +1,7 @@
 
-set(CROW_LINK_LIBRARIES Crow::Crow)
+set(CROW_LINK_LIBRARIES Crow::Crow ZLIB::ZLIB)
+add_definitions(-DCROW_ENABLE_COMPRESSION)
+add_definitions(-DCROW_DISABLE_STATIC_DIR)
 find_package(Crow REQUIRED)
+find_package(ZLIB REQUIRED)
 message("Found Crow")
